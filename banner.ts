a832ca16809fbbac71855df646550695afaf1459
DevAbabil <ababilhossain35@gmail.com>
2025-04-16 11:04:21 +0600

    chore: add banner.ts for build output headers

---
+++ banner.ts
@@ -0,0 +1,16 @@
+/**
+ * @packageName     siteviews
+ * @description     A zero-config, plug-and-play website views counter.
+ * @author          Md Ababil Hossain (https://devababil.com)
+ * @website         https://siteviews.netlify.app
+ * @license         MIT
+ * @version         2.0.0
+ * @repository      https://github.com/DevAbabil/siteviews
+ * @created         2024-11-10
+ * @lastModified    2025-04-16
+ * ------------------------------------------------------------
+ * 🧠 Built with passion and simplicity in mind.
+ * 🚀 Lightweight, flexible, and developer-friendly.
+ * 💡 Contributions are welcome!
+ * ------------------------------------------------------------
+ */
