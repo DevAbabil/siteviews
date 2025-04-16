52ad1f7167bd742468054faac53c5ae18726a7ae
DevAbabil <ababilhossain35@gmail.com>
2025-04-16 11:05:49 +0600

    chore(tsup.config.ts): add banner.ts for package banner injection

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "tsup";
+import fs from "fs";
 
 export default defineConfig([
   {
     entry: ["src/index.ts"],
     format: ["cjs"],
+    banner: {
+      js: fs.readFileSync("./banner.ts", "utf-8"),
+    },
   },
 ]);
