6a2f1b0f70c1f9a55ff7f6202305eb8de3e8c759
DevAbabil <ababilhossain35@gmail.com>
2025-06-05 07:02:16 +0600

    chore: bump version to 2.0.2

--- banner.ts
+++ banner.ts
@@ -4,7 +4,7 @@
  * @author          Md Ababil Hossain (https://devababil.com)
  * @website         https://siteviews.netlify.app
  * @license         MIT
- * @version         2.0.1
+ * @version         2.0.2
  * @repository      https://github.com/DevAbabil/siteviews
  * @created         2024-11-10
  * @lastModified    2025-06-05
